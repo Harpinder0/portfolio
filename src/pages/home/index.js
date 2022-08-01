2b9f3344154b210fa0d87dcee6999a431d4a5686
harpinder singh <harpinderbhullar84@gmail.com>
2022-08-01 14:11:16 +0530

    pages & routes declare

---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from '../../Component/Home';
+
+const home = () => {
+  return (
+    <Home />
+  )
+}
+
+export default home;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,52 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from "../Component/Navbar";
 import CvDownload from "../Component/CvDownload";
 
+// Pages
+import Home from "../pages/home";
+import About from "../pages/about";
+import Service from "../pages/service";
+import Project from "../pages/project";
+import Contact from "../pages/contact";
+
 const routes = [
     {
         key: 1,
+        path: '/home',
+        component: Home,
+        exact: true
+    },
+    {
+        key: 2,
+        path: '/about',
+        component: About,
+        exact: true
+    },
+    {
+        key: 3,
+        path: '/service',
+        component: Service,
+        exact: true
+    },
+    {
+        key: 4,
+        path: '/project',
+        component: Project,
+        exact: true
+    },
+    {
+        key: 5,
+        path: '/contact',
+        component: Contact,
+        exact: true
+    },
+    {
+        key: 6,
         path: '/portfolio',
         component: Navbar,
         exact: true
     },
     {
-        key: 2,
+        key: 7,
         path: '/cvdownload',
         component: CvDownload,
         exact: true
