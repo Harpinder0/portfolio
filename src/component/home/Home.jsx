3554f12d43894e5a75947de25d98ac61218d0d69
harpinder singh <harpinderbhullar84@gmail.com>
2022-09-08 14:06:40 +0530

    implement github api && fetch info

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { NavLink } from 'react-router-dom';
 import { FaFacebookF } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
@@ -6,17 +6,31 @@ import { GrLinkedinOption } from "react-icons/gr";
 import { AiOutlineTwitter } from "react-icons/ai";
 import user1 from '../../images/usermy.jpg'
 import Cv from '../../images/HarpinderSingh.pdf'
+
+// api
+import { github_User_Info } from '../../api/apis';
+
+// style
 import './style.css';
 
-
 const Home = () => {
+
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    github_User_Info().then((res) => {
+      // console.log(res);
+      setUserInfo(res);
+    })
+  }, [])
+
   return (
     <section className="banner" id="banner">
       <div className="content">
         <div className="imgBx">
-          <img src={user1} alt="user" />
+          <img src={userInfo?.avatar_url || user1} alt="user" />
         </div>
-        <h3>Harpinder Singh</h3>
+        <h3>{userInfo?.name || "Harpinder Singh" }</h3>
         <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
         {/* <NavLink to="/cvdownload" activeClassName="selected" className="btn">Download My CV</NavLink> */}
         <a href={Cv} className="btn">Download My CV</a>
